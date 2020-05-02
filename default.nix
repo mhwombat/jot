8c03aaea11809b647ef098d04b0d026eea4facbf
Amy de Buitléir <amy@nualeargais.ie>
2020-05-02 19:37:09 +0100

    using nix + cabal

--- default.nix
+++ default.nix
@@ -1,14 +1,31 @@
-{ mkDerivation, base, cmdargs, directory, filepath, process, stdenv
-, time
-}:
-mkDerivation {
-  pname = "Jot";
-  version = "1.2";
-  src = ./.;
-  isLibrary = false;
-  isExecutable = true;
-  executableHaskellDepends = [
-    base cmdargs directory filepath process time
-  ];
-  license = stdenv.lib.licenses.publicDomain;
-}
+{ nixpkgs ? import ./nix/nixos-20-03.nix }:
+let
+  overlay = self: super: {
+    myHaskellPackages = 
+      super.haskell.packages.ghc865.override (old: {
+        overrides = self.lib.composeExtensions (old.overrides or (_: _: {})) 
+          (hself: hsuper: {
+            ghc = hsuper.ghc // { withPackages = hsuper.ghc.withHoogle; };
+            ghcWithPackages = hself.ghc.withPackages;
+          });
+      });
+  };
+
+  pkgs = import nixpkgs {
+    overlays = [overlay];
+  };
+
+  drv = pkgs.myHaskellPackages.callCabal2nix "jot" ./jot.cabal {};
+
+  drvWithTools = drv.env.overrideAttrs (
+    old: with pkgs.myHaskellPackages; {
+      nativeBuildInputs = old.nativeBuildInputs ++ [
+        ghcid
+        # Add other development tools like ormolu here
+      ];
+      shellHook = ''
+         source .config/secrets     '';
+      }
+  );
+in
+  drvWithTools
