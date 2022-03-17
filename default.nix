a3b4f717ca16b6100c095caab85ad14cb42735bf
Amy de Buitléir <amy@nualeargais.ie>
2022-03-17 15:30:34 +0000

    test

--- default.nix
+++ default.nix
@@ -1,31 +1,6 @@
-{ nixpkgs ? import ./nix/nixos-20-03.nix }:
-let
-  overlay = self: super: {
-    myHaskellPackages = 
-      super.haskell.packages.ghc865.override (old: {
-        overrides = self.lib.composeExtensions (old.overrides or (_: _: {})) 
-          (hself: hsuper: {
-            ghc = hsuper.ghc // { withPackages = hsuper.ghc.withHoogle; };
-            ghcWithPackages = hself.ghc.withPackages;
-          });
-      });
-  };
-
-  pkgs = import nixpkgs {
-    overlays = [overlay];
-  };
-
-  drv = pkgs.myHaskellPackages.callCabal2nix "jot" ./jot.cabal {};
-
-  drvWithTools = drv.env.overrideAttrs (
-    old: with pkgs.myHaskellPackages; {
-      nativeBuildInputs = old.nativeBuildInputs ++ [
-        ghcid
-        # Add other development tools like ormolu here
-      ];
-      shellHook = ''
-         source .config/secrets     '';
-      }
-  );
-in
-  drvWithTools
+let 
+  pkgs = import <nixpkgs> { };
+in 
+  pkgs.haskellPackages.developPackage {
+    root = ./.;
+  }
